e43a7886bc9a13cdd0bfbb5ae9cdff8ae6628b40
krulikovskiy <it@krulikovskiy.ru>
2018-10-23 12:47:16 +0500

    Update firststart.sh

--- firststart.sh
+++ firststart.sh
@@ -3,6 +3,10 @@
 # Запускать под ROOT
 # $1 - название проекта, при повторном создании проекта запускать файл project.sh, а не init.sh
 
+# Нужные пакеты для python
+apt-get -y update
+apt-get -y install xz-utils gcc libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev nginx
+
 # Установка Python 3.6.7
 wget https://www.python.org/ftp/python/3.6.7/Python-3.6.7.tar.xz
 tar -xpJf Python-3.6.7.tar.xz
@@ -10,6 +14,7 @@ cd Python-3.6.7
 ./configure --enable-optimizations
 make altinstall
 
+cd ..
 # Удаление установочных файлов Python
 rm -R Python-3.6.7
 rm Python-3.6.7.tar.xz
